e0de497ee5ba74007dbbf798050a17e456a147a4
Kareem <kareem.karimulla@tcs.com>
2017-03-03 16:58:20 +0530

    update

--- angularjs2/src/app/app.component.ts
+++ angularjs2/src/app/app.component.ts
@@ -1,29 +1,29 @@
 import { Component } from '@angular/core';
-
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice2' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
 
 @Component({
   selector: 'my-app',
    templateUrl: './view/app.component.html',
-  styleUrls:['./view/app.component.css']
+  styleUrls:['./view/app.component.css'],
+   providers: [HeroService]
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
