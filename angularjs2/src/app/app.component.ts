cce5827b33614b168b4dc58fd79c8a8e63876ca0
Kareem <kareem.karimulla@tcs.com>
2017-03-09 17:06:57 +0530

    http service updated

--- angularjs2/src/app/app.component.ts
+++ angularjs2/src/app/app.component.ts
@@ -1,31 +1,12 @@
 import { Component } from '@angular/core';
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
-
 
 @Component({
-  selector: 'my-app',
-   templateUrl: './view/app.component.html',
-  styleUrls:['./view/app.component.css'],
-   providers: [HeroService]
+    selector: 'my-app',
+    templateUrl: './view/app.component.html',
+    styleUrls : ['./view/app.component.css']
 })
 
-export class AppComponent implements OnInit {
-  title = 'Tour of Heroes';
-  heroes: Hero[];
-  selectedHero: Hero;
 
-  constructor(private heroService: HeroService) { }
-
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+export class AppComponent {
+    title = 'Tours of Heroes';
 }
