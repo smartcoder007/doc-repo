201b63c2a6e1e3dbcc518a882ba5399ced01c572
kareemmca <kareem@gmail.com>
2019-01-12 20:02:09 +0530

    refactor

--- sparksamples/src/main/scala/com/kareem/spark/WordCount.scala
+++ sparksamples/src/main/scala/com/kareem/spark/WordCount.scala
@@ -4,7 +4,6 @@ import org.apache.spark.SparkContext._
 import org.apache.log4j.Logger
 import org.apache.spark.rdd.RDD.rddToPairRDDFunctions
 
-
 object WordCount {
   def main(args: Array[String]) {
 
@@ -17,8 +16,8 @@ object WordCount {
     val counts = textFile.flatMap(line => line.split(" "))
       .map(word => (word, 1))
       .reduceByKey(_ + _)
-      println(counts.collect.foreach(println))
-     
+    println(counts.collect.foreach(println))
+
     counts.saveAsTextFile("C://test2.txt")
   }
 
